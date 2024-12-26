5a1e3cf199e0e126824ae7772b4c82f3388240a1
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2024-12-26 08:41:40 +0700

    fix: update service worker source path in next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -8,7 +8,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
   // use something else that works, such as "service-worker/index.ts".
-  swSrc: "src/service-worker/app-worker.ts",
+  swSrc: "service-worker/app-worker.ts",
   swDest: "public/sw.js",
 });
 
